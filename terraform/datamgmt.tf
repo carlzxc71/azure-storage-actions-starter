4aad3bda9cae6394b96cfa042b779e08e7d1fc34
Carl Lindberg <carlzxc71@gmail.com>
2024-04-11 20:34:05 +0200

    Add storage task azapi resource

---
+++ terraform/datamgmt.tf
@@ -0,0 +1,40 @@
+resource "azapi_resource" "app" {
+  type                      = "Microsoft.StorageActions/storageTasks@2023-01-01"
+  name                      = "mystoragetask"
+  parent_id                 = azurerm_resource_group.this.id
+  schema_validation_enabled = false
+
+  body = jsonencode({
+    location = azurerm_resource_group.this.location
+    properties = {
+      enabled     = true,
+      description = "Storage task",
+      action = {
+        if = {
+          condition = "[[endsWith(Name, '.xlsx')]]",
+          operations = [
+            {
+              name = "SetBlobTier",
+              parameters = {
+                tier = "Cool"
+              },
+              onSuccess = "continue",
+              onFailure = "break"
+            },
+            {
+              name = "SetBlobTags",
+              parameters = {
+                TierUpdatedBy = "StorageTask"
+              },
+              onSuccess = "continue",
+              onFailure = "break"
+            }
+          ]
+        }
+      },
+    }
+    identity = {
+      type = "SystemAssigned"
+    }
+  })
+}
